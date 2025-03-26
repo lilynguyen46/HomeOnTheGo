74a82f8f4a52f7f14e30f8c9dfb3a9c64c727764
Eduardo <eduardoherrerabarra@gmail.com>
2025-03-26 15:51:23 -0400

    made crud

---
+++ CRUD/HomeOnTheGo/src/test/java/HomeOnTheGo/HomeOnTheGo/HomeOnTheGoApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeOnTheGo.HomeOnTheGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeOnTheGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
