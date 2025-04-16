56bec981b962cd10520d310d365a09384dec727f
Lily Griggs <lilygriggs@wireless-152-13-60-236.uncg.edu>
2025-04-16 18:20:04 -0400

    Reviews

--- src/main/java/HomeOnTheGo/HomeOnTheGo/Service/PropertyService.java
+++ src/main/java/HomeOnTheGo/HomeOnTheGo/Service/PropertyService.java
@@ -5,23 +5,37 @@ import HomeOnTheGo.HomeOnTheGo.Repository.PropertyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import HomeOnTheGo.HomeOnTheGo.Model.Review;
-
+import HomeOnTheGo.HomeOnTheGo.Model.Tenant;
+import java.util.List;
 import java.time.LocalDate;
 import java.util.Collections;
 import HomeOnTheGo.HomeOnTheGo.Repository.ReviewRespository;
+import HomeOnTheGo.HomeOnTheGo.Repository.TenantRepository;
 @Service
 @RequiredArgsConstructor
 public class PropertyService {
     private final PropertyRepository propertyRepository;
     private final ReviewRespository reviewRespository;
-    public Review createReviewForProperty(Long propertyId, Review reviewData) {
+    private final TenantRepository tenantRepository;
+    public Review createReviewForProperty(Long propertyId, Review reviewData, Long tenantId) {
         Property property = propertyRepository.findById(propertyId)
                 .orElseThrow(() -> new RuntimeException("Property not found"));
+        Tenant tenant = tenantRepository.findById(tenantId)
+                .orElseThrow(() -> new RuntimeException("Tenant not found"));
         reviewData.setCreatedAt(LocalDate.now());
         reviewData.setProperty(property);
+        reviewData.setTenant(tenant);
+
         property.setReview(Collections.singletonList(reviewData));
         return reviewRespository.save(reviewData);
         
+
+
+    }
+
+
+    public List<Review> getAllReviewsForProperty(Long propertyId) {
+        return reviewRespository.findByPropertyId(propertyId);
     }
 
 }
